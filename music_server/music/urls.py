a8e4e3f856b5b646a763b0f4453f143d97a34c13
Aniketh <anikethbhosale11@gmail.com>
2025-11-25 07:38:41 +0000

    Implement basic structure for music server: add base template, song and playlist views, and corresponding URL patterns. Remove unused playlist detail template.

--- music_server/music/urls.py
+++ music_server/music/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'music'  # app name ti tell Django the namespace for this app
+
 urlpatterns = [
     # Add your URL patterns here
+    path('songs/', views.song_list, name='song_list'),
+    path('playlists/', views.playlist_list, name='playlists'),
+    #path('player/', views.player, name='player'),
 ]
--- music_server/music/views.py
+++ music_server/music/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Song, Playlist
 # Create your views here.
+
+
+def song_list(request):
+    songs = Song.objects.all()
+    return render(request, 'songs_list.html', {'songs': songs})
+
+def playlist_list(request): 
+    playlists = Playlist.objects.all()
+    return render(request, 'playlists_list.html', {'playlists': playlists})
